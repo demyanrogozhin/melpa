3e7815c43b9fa0ce00fbe5a09f2f1903bcdceff0
Donald Curtis <dcurtis@milkbox.net>
2013-09-14 17:15:43 -0700

    Allow for builds to run simultaneously.
    By recording each package's archive-entry separately we are able to
    build each recipe independently and then compile the archive contents
    afterwards.

--- missing.el
+++ missing.el
@@ -1,28 +1,27 @@
-#!/bin/sh 
+#!/bin/sh
 :;exec emacs --script "$0" "$@"
 
-(defun difference (left right)
-  "compare two lists"
-  (let ((caleft (car left))
-        (caright (car right)))
-    (cond
-     ((not left) right)
-     ((not right) left)
-     ((string< caleft caright)
-      (cons caleft (difference (cdr left) right)))
-     ((string< caright caleft)
-      (cons caright (difference left (cdr right))))
-     (t (difference (cdr left) (cdr right))))))
+(defun missing-packages (recipes packages)
+  "Show elements of RECIPES that are no in PACKAGES."
+  (let (missing)
+    (while recipes
+      (let ((recipe (car recipes))
+            (package (car packages)))
+        (cond
+         ((or (not package) (string< recipe package))
+          (setq missing (cons recipe missing)))
+         ((string< package recipe)
+          (error "Package has no recipe: %s" package))
+         (t (setq packages (cdr packages)))))
+      (setq recipes (cdr recipes)))
+    (reverse missing)))
 
 (defun stripstuff (fn)
-  "strip the date and extension"
-  (string-match "\\\(.*\\\)-[0-9]+\.\\\(el$\\\|tar$\\\)" fn)
+  "Strip the date and extension from FN."
+  (string-match "\\(.*\\)-[0-9]+\\.[0-9]+\\.\\(el$\\|tar$\\)" fn)
   (match-string 1 fn))
 
-(mapc 'message
-      (difference
-       (sort (directory-files "recipes/" nil "[^.].*") 'string<)
-       (sort (mapcar 'stripstuff (directory-files "packages/" nil "[^.].*\\\(el$\\\|tar$\\\)")) 'string<)))
-
-
-
+(mapc 'print
+      (missing-packages
+       (sort (directory-files "recipes/" nil "^[^.].*") 'string<)
+       (sort (mapcar 'stripstuff (directory-files "packages/" nil "^[^.].*\\\(el$\\\|tar$\\\)")) 'string<)))
